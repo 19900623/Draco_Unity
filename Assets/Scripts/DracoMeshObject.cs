c46bc96d563f1043f3284a6a4e7de438e0036026
zfx29 <zfx29@LIVE.COM>
2018-06-20 21:59:07 +0800

    commit message

---
+++ Assets/Scripts/DracoMeshObject.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class DracoMeshObject : MonoBehaviour
+{
+	public string AssetName = "out";
+    // This function will be used when the GameObject is initialized.
+    void Start()
+	{
+        // If the original mesh exceeds the limit of number of verices, the
+        // loader will split it to a list of smaller meshes.
+        List<Mesh> meshes = new List<Mesh>();
+		DracoMeshLoader dracoLoader = new DracoMeshLoader();
+        /*
+         * Here we use the compressed Bunny model as example.
+         * It's in unity/Resources/bunny.drc.bytes.
+         * Please see README.md for details.
+         */
+        int numFaces = dracoLoader.LoadMeshFromAsset(AssetName, ref meshes);
+
+
+        /* Note: You need to add MeshFilter (and MeshRenderer) to your GameObject.
+         * Or you can do something like the following in script:
+         * AddComponent<MeshFilter>();
+         */
+        if (numFaces > 0)
+		{
+			MeshFilter filter = GetComponent<MeshFilter>();
+			if(filter == null)
+			{
+				filter = gameObject.AddComponent<MeshFilter>();
+			}
+			filter.mesh = meshes[0];
+		}
+
+		if(GetComponent<MeshRenderer>() == null)
+		{
+			MeshRenderer renderer = gameObject.AddComponent<MeshRenderer>();
+			renderer.material = new Material(Shader.Find("Legacy Shaders/Diffuse"));;
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,52 @@
+# Draco_Unity
+Unity 2018.1.0f2
+
+[Google Draco](https://github.com/google/draco)
+
+[Google Draco Unity](https://github.com/google/draco/tree/master/unity)
+
+> Draco 
+>
+> Editor
+>
+> Plugins
+>
+> Resources
+>
+> Scenes
+>
+> Scripts
+
+Windows10 环境 Draco 编译步骤 https://igco.xyz/d/14-google-draco 
+
+- CMake 
+- Windows 10 
+- Visual Studio 2017
+
+![](https://github.com/iifork/Draco_Unity/blob/master/1.png?raw=true)
+
+------
+
+----
+
+Draco Release （ draco_encoder.exe - 编译后的可执行文件 Windows10 ）
+
+编译成功的可执行文件：https://github.com/iifork/Draco_Unity/releases
+
+[使用方法](https://igco.xyz/d/14-google-draco/8) 
+
+CMD: ` draco_encoder.exe `
+
+`draco_encoder.exe -o "xxx.drc" -i "xxx.obj" `
+
+由于 Unity 只能识别 Unity 已知的文件扩展名，因此需要将压缩的 `.drc` 文件更改为 `.bytes ` ，以便 Unity 将其识别为二进制文件。例如:
+
+`xxx.obj` 要改成 `xxx.bytes ` 才能被Unity 读取
+
+------
+
+#### **WELCOME**
+
+#### To 	 [IGC - Indie Game Community .](https://igco.xyz/) ( Chinese Community )
+
+[iifork.com](https://iifork.com) ✔
