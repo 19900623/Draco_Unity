d5679efe63e404689b9aa0bf11db365bfc8f5467
DESKTOP-62EQ6VC\zfx29 <zfx29@LIVE.COM>
2018-06-20 14:40:47 +0800

    Draco_Unity

---
+++ Assets/Scripts/DracoMeshObject.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class DracoMeshObject : MonoBehaviour
+{
+	public string AssetName = "te";
+
+	void Start()
+	{
+		List<Mesh> meshes = new List<Mesh>();
+		DracoMeshLoader dracoLoader = new DracoMeshLoader();
+
+		int numFaces = dracoLoader.LoadMeshFromAsset(AssetName, ref meshes);
+
+		if(numFaces > 0)
+		{
+			MeshFilter filter = GetComponent<MeshFilter>();
+			if(filter == null)
+			{
+				filter = gameObject.AddComponent<MeshFilter>();
+			}
+			filter.mesh = meshes[0];
+		}
+
+		if(GetComponent<MeshRenderer>() == null)
+		{
+			MeshRenderer renderer = gameObject.AddComponent<MeshRenderer>();
+			renderer.material = new Material(Shader.Find("Legacy Shaders/Diffuse"));;
+		}
+	}
+}
